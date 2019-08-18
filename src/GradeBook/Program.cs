53648717bf34015c181de9f699e405f7bb621e13
Hilal Tarakci <hilal1.tarakci@ge.com>
2019-08-18 11:53:40 -0500

    add check

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,8 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {args[0]}!");
+            if(args.Length > 0) Console.WriteLine($"Hello {args[0]}!");
+            else Console.WriteLine("Hello there!");
         }
     }
 }
